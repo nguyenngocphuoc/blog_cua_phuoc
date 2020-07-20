ea37df4e7d00fd6cff593ca298ed2efbaa7b003f
NgDuyPhong <NgDuyPhong@users.noreply.github.com>
2020-07-20 19:48:32 +0700

    phong done code phone

--- resources/views/view/masterPage.blade.php
+++ resources/views/view/masterPage.blade.php
@@ -151,6 +151,25 @@
     @yield('content')
     <!-- ##### Mag Posts Area End ##### -->
 
+    <div class="hotline-phone-ring-wrap">
+        <div class="hotline-phone-ring">
+            <div class="hotline-phone-ring-circle"></div>
+            <div class="hotline-phone-ring-circle-fill"></div>
+            <div class="hotline-phone-ring-img-circle">
+                <a href="tel:0987654321" class="pps-btn-img">
+                    <img src="https://nguyenhung.net/wp-content/uploads/2019/05/icon-call-nh.png" alt="Gọi điện thoại"
+                        width="50">
+                </a>
+            </div>
+        </div>
+        <div class="hotline-bar">
+            <a href="tel:0947467073">
+                <span class="text-hotline">0947.467.073</span>
+            </a>
+        </div>
+    </div>
+
+
     <!-- ##### Footer Area Start ##### -->
     <footer class="footer-area">
         <div class="container">
